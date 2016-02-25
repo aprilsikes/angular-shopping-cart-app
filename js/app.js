d6edd72a1bf7e7ebe416ee442b43292edffd2539
april sikes <aprilsikes@gmail.com>
2016-02-25 16:43:44 -0700

    app setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module('april', []);
+var app = angular.module('april', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+      .when('/goodbye', {
+        templateUrl: 'partials/goodbye.html',
+        controller: 'GoodbyeController'
+      })
+      .when('/hello', {
+        templateUrl: 'partials/hello.html',
+        controller: 'HelloController'
+      })
+});
