109fe109ccea7272b240eefa7e100e1f2cdb2426
april sikes <aprilsikes@gmail.com>
2016-02-27 17:44:07 -0600

    derpy

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,4 +1,5 @@
 app.controller('MainController', ['$scope', 'teaService', function ($scope, teaService) {
+  $scope.buttonValue = 'Empty';
   console.log('Getting this!');
   teaService.getTea().then(function (data) {
     $scope.teas = data.data;
