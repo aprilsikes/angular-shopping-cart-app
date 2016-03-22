a4724142244015d99cf456a708c6ff970be6bae7
april sikes <aprilsikes@gmail.com>
2016-03-21 21:19:33 -0600

    by god i think i've got it

--- js/services/tea_service.js
+++ js/services/tea_service.js
@@ -7,8 +7,8 @@ app.service('teaService', ['$http', function ($http) {
     },
 
     addItem: function(tea, qty) {
-      qty == undefined ? tea.quantity = 1 : tea.quantity = Number(qty)
-      cart.push(tea, qty);
+      qty == undefined ? tea.qty = 1 : tea.qty = Number(qty)
+      cart.push(tea);
       console.log(cart);
       return cart;
     },
@@ -25,7 +25,7 @@ app.service('teaService', ['$http', function ($http) {
         var total = 0;
         var array = this.getItems();
         for (var i = 0; i < array.length; i++) {
-          total += (array[i].quantity * array[i].price)/100;
+          total += (array[i].qty * array[i].price)/100;
         }
         return '$'+total.toFixed(2);
     }
